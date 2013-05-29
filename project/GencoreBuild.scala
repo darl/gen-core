84a477bb1f735b1c05f7136fdd06d6df71426838
Vladislav Dolbilov <darl@yandex-team.ru>
2013-05-30 01:43:36 +0400

    project skeleton

---
+++ project/GencoreBuild.scala
@@ -0,0 +1,19 @@
+import sbt._
+import sbt.Keys._
+
+object GencoreBuild extends Build {
+
+  lazy val gencore = Project(
+    id = "gen-core",
+    base = file("."),
+    settings = Project.defaultSettings ++ Seq(
+      name := "gen-core",
+      organization := "com.github.darl",
+      version := "0.1-SNAPSHOT",
+      scalaVersion := "2.10.1",
+      // add other settings here
+      libraryDependencies += "org.scala-lang" % "scala-reflect" % "2.10.1",
+      libraryDependencies += "org.scala-lang" % "scala-compiler" % "2.10.1"
+    )
+  )
+}
